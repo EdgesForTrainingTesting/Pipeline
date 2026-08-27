f86b95287bdda33864d949203e54223b66563565
Moataz <moatazelngar23@gmail.com>
2026-08-27 21:17:32 +0300

    Fix Error

--- app/src/main/java/demo/model/Task.java
+++ app/src/main/java/demo/model/Task.java
@@ -18,7 +18,8 @@ public class Task {
     private String test;
     private int feature1;
     private int feature2;
-    private int feature3
+    private int feature3;
+    private int feature4;
     public Task(String title, String description, TaskPriority priority) {
         this.title = title;
         this.description = description;
